b41aa50b5768d1805619e9d88ff42b835f93d815
Samuel Georges <sam@daftspunk.com>
2021-06-16 19:38:20 +1000

    Share bindings with parent container

--- src/Foundation/Maker.php
+++ src/Foundation/Maker.php
@@ -12,11 +12,6 @@ use ReflectionParameter;
 class Maker
 {
     /**
-     * @var array bindings for the container
-     */
-    protected $bindings = [];
-
-    /**
      * @var Container container for the application
      */
     protected $container;
@@ -45,7 +40,7 @@ class Maker
      */
     public function bind($abstract, Closure $concrete)
     {
-        $this->bindings[$abstract] = $concrete;
+        $this->container->bind($abstract, $concrete);
     }
 
     /**
@@ -151,7 +146,12 @@ class Maker
      */
     protected function getBinding($abstract)
     {
-        return $this->isBound($abstract) ? $this->bindings[$abstract] : $abstract;
+        if (!$this->isBound($abstract)) {
+            return $abstract;
+        }
+
+        return $this->container->getBindings()[$abstract]['concrete'];
+
     }
 
     /**
@@ -161,7 +161,7 @@ class Maker
      */
     protected function isBound($abstract)
     {
-        return array_key_exists($abstract, $this->bindings);
+        return $this->container->bound($abstract);
     }
 
     /**
